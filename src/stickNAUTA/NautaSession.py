2000b909aea01e01a93300bb168b60bf508bee21
Julio C. Galindo <jcgalindo.jcgh@gmail.com>
2021-04-12 18:47:13 -0400

    Added get/set session_data so the session can be recovered

--- src/stickNAUTA/NautaSession.py
+++ src/stickNAUTA/NautaSession.py
@@ -3,8 +3,10 @@ from __future__ import (annotations)
 from ctypes import (Union)
 from re import (search)
 
+from json import (load, dump)
 from lxml import (html)
 from requests import (Session)
+from requests.utils import (dict_from_cookiejar, cookiejar_from_dict)
 
 
 class NautaSession(object):
@@ -170,3 +172,46 @@ class NautaSession(object):
             remaining_time = hours * 3600 + minutes * 60 + seconds
 
         return remaining_time
+
+    def get_session_data(self) -> dict:
+        if not self.__logged_in:
+            raise RuntimeError('Cannot get session data since user is not logged in.')
+
+        session_data = {
+            'username': self.__username,
+            'cookies': dict_from_cookiejar(self.__session.cookies),
+            'wlanuserip': self.__wlanuserip,
+            'CSRFHW': self.__CSRFHW,
+            'ATTRIBUTE_UUID': self.__ATTRIBUTE_UUID
+        }
+        return session_data
+
+    def set_session_data(self, session_data: dict) -> None:
+        if self.__logged_in:
+            raise RuntimeError('Cannot set session data since user is logged in.')
+
+        required_keys = ['username', 'cookies', 'wlanuserip', 'CSRFHW', 'ATTRIBUTE_UUID']
+        for key in required_keys:
+            if key not in session_data.keys():
+                raise ValueError(f'session_data kas not required key: \'{key}\'.')
+
+        if not session_data['username'] == self.__username:
+            raise ValueError('Session data is not for this account.')
+
+        self.__session.cookies = cookiejar_from_dict(session_data['cookies'])
+        self.__wlanuserip = session_data['wlanuserip']
+        self.__CSRFHW = session_data['CSRFHW']
+        self.__ATTRIBUTE_UUID = session_data['ATTRIBUTE_UUID']
+        self.__logged_in = True
+
+    def save_session_data_to_file(self, file_path: str) -> None:
+        with open(file_path, 'w') as file:
+            dump(self.get_session_data(), file)
+
+    def load_session_data_from_file(self, file_path: str) -> None:
+        with open(file_path, 'r') as file:
+            session_data = load(file)
+            if isinstance(session_data, dict):
+                self.set_session_data(session_data)
+            else:
+                raise ValueError('File does not contain a dict and therefore not a session data.')
